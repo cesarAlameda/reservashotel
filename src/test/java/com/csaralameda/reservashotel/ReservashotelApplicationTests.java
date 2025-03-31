be72fb18969220ccd581c286c94108e5df722ccc
cesarAlameda <147344298+cesarAlameda@users.noreply.github.com>
2025-03-31 17:26:44 +0200

    init commit

---
+++ src/test/java/com/csaralameda/reservashotel/ReservashotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaralameda.reservashotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservashotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
